ba40b6042723c73e5da039d33e2a304d728b153b
Breno Camargos <breno.diogo@hotmail.com>
2026-02-08 13:53:19 +0100

    Initial commit

---
+++ src/main/kotlin/com/sgchallenge/f1/F1BettingApplication.kt
@@ -0,0 +1,11 @@
+package com.sgchallenge.f1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class F1BettingApplication
+
+fun main(args: Array<String>) {
+    runApplication<F1BettingApplication>(*args)
+}
